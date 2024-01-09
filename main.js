43bd851b00e4de00f8191f93652e5adb7d92a02f
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-09 17:17:00 +0100

    completed (no append)

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-const ul=document.querySelector('ul.list')
+//Declaration
+const cell=document.querySelector('div.list')
 
+//CICLO FOR
 for(let i=1; i<=100; i++){
 
-    const element= `<li> ${i} </li>`;
+    const element= `<div> ${i} </div>`;
 
+    //Multiple of 5 and 3
     if( i %3 == 0 && i %5 == 0){
-        ul.innerHTML += '<li> Buzz Fizz </li>'
-
-    }else if(i %3 == 0 ){
-        ul.innerHTML += '<li> Fizz </li>'
-
-    }else if(i %5 == 0 ){
-        ul.innerHTML += '<li> Buzz </li>'
-        
-    }else {
-        ul.innerHTML += element
+        cell.innerHTML += '<div class="col-2 py-4 border text-center shadow-lg border-danger rounded" > Buzz Fizz </div>'
+        console.log('buzz fizz')
     }
+    //Multiple only of 3
+    else if(i %3 == 0 ){
+        cell.innerHTML += '<div class="col-2 py-4 border text-center shadow-lg border-warning rounded" > Fizz </div>'
+        console.log('fizz')
+
+    }
+    //Multiple only of 5
+    else if(i %5 == 0 ){
+        cell.innerHTML += '<div class="col-2 py-4 border text-center shadow-lg border-success rounded" > Buzz </div>'
+        console.log('buzz')
+
+    }
+    //All other numbers
+    else {
+        cell.innerHTML += `<div class="col-2 py-4 border text-center shadow-lg border-primary rounded" > ${element} </div>`
+        console.log(element)
+    }
+
 }
